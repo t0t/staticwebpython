07b5b305c6ecbbc7fc07f33f717703d88979dd2a
Sergio Forés <sergiofores@gmail.com>
2021-02-04 18:08:08 +0100

    probando deploy

--- freeze.py
+++
@@ -1,8 +0,0 @@
-from flask_frozen import Freezer
-from app import app
-
-# Create a Freezer instance with your app object and call its freeze() method.
-freezer = Freezer(app)
-
-if __name__ == '__main__':
-    freezer.freeze()
